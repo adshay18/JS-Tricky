82e3d114057888f7c01eca485e3e73c8ef24412c
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-26 19:08:58 -0400

    write guessing game

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,22 @@
 function guessingGame() {
-
+	let number = Math.floor(Math.random() * 100);
+	let winner = false;
+	let count = 0;
+	return function guess(num) {
+		if (winner) return 'The game is over, you already won!';
+		if (num < number) {
+			count++;
+			return `${num} is too low!`;
+		} else if (num > number) {
+			count++;
+			return `${num} is too high!`;
+		}
+		if (num === number) {
+			count++;
+			winner = true;
+			return `You win! You found ${number} in ${count} guesses.`;
+		}
+	};
 }
 
 module.exports = { guessingGame };
