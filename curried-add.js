df6bd50e9b23b6f14e58eb4be6749b0b6995dc43
Andrew Shay <shay@Andrews-MacBook-Pro.local>
2022-05-26 22:33:29 -0400

    write curried

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,10 @@
-function curriedAdd(total) {
-
+function curriedAdd(total = 0) {
+	if (arguments.length === 0) return total;
+	return function add(num) {
+		if (num === undefined) return total;
+		total += num;
+		return add;
+	};
 }
 
 module.exports = { curriedAdd };
